5eca7bf88e12594b69e87777d90b65e9d172d7ea
sinchana-31 <sinchupattarr@gmail.com>
2026-05-29 22:24:06 +0530

    Fix variable declaration for temp in bubblesort

--- bubblesort.c
+++ bubblesort.c
@@ -3,10 +3,11 @@
 void bubblesort(int arr[], int n)
 {
   int i, j;
+  int temp;
   for(i=0;i<n-1;i++){
     for(j=0;i<n-i-1; j++){
       if(arr[j]>arr[j+1]){
-        int temp=arr[j];
+         temp=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=temp;
       }
